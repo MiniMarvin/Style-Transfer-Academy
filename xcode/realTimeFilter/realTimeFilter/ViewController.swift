6ae0a0b330ec2c7c974a84cc039a7bee903fb98b
Caio Gomes <caiogomes34@gmail.com>
2019-05-24 15:33:59 -0300

    xcode stuff

---
+++ xcode/realTimeFilter/realTimeFilter/ViewController.swift
@@ -0,0 +1,207 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+
+
+import UIKit
+import AVFoundation
+import CoreML
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    var captureSession = AVCaptureSession()
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    var orientation: AVCaptureVideoOrientation = .portrait
+    
+    let context = CIContext()
+    
+    @IBOutlet weak var filteredImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupDevice()
+        setupInputOutput()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        orientation = AVCaptureVideoOrientation(rawValue: UIApplication.shared.statusBarOrientation.rawValue)!
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) != .authorized
+        {
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler:
+                { (authorized) in
+                    DispatchQueue.main.async
+                        {
+                            if authorized
+                            {
+                                self.setupInputOutput()
+                            }
+                    }
+            })
+        }
+    }
+    
+    func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            }
+            else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+        
+        currentCamera = backCamera
+    }
+    
+    func setupInputOutput() {
+        do {
+            setupCorrectFramerate(currentCamera: currentCamera!)
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            captureSession.sessionPreset = AVCaptureSession.Preset.hd1280x720
+            if captureSession.canAddInput(captureDeviceInput) {
+                captureSession.addInput(captureDeviceInput)
+            }
+            let videoOutput = AVCaptureVideoDataOutput()
+            
+            videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer delegate", attributes: []))
+            if captureSession.canAddOutput(videoOutput) {
+                captureSession.addOutput(videoOutput)
+            }
+            captureSession.startRunning()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupCorrectFramerate(currentCamera: AVCaptureDevice) {
+        for vFormat in currentCamera.formats {
+            //see available types
+            //print("\(vFormat) \n")
+            
+            var ranges = vFormat.videoSupportedFrameRateRanges as [AVFrameRateRange]
+            let frameRates = ranges[0]
+            
+            do {
+                //set to 240fps - available types are: 30, 60, 120 and 240 and custom
+                // lower framerates cause major stuttering
+                if frameRates.maxFrameRate == 240 {
+                    try currentCamera.lockForConfiguration()
+                    currentCamera.activeFormat = vFormat as AVCaptureDevice.Format
+                    //for custom framerate set min max activeVideoFrameDuration to whatever you like, e.g. 1 and 180
+                    currentCamera.activeVideoMinFrameDuration = frameRates.minFrameDuration
+                    currentCamera.activeVideoMaxFrameDuration = frameRates.maxFrameDuration
+                }
+            }
+            catch {
+                print("Could not set active format")
+                print(error)
+            }
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        connection.videoOrientation = orientation
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+        
+        let comicEffect = CIFilter(name: "CIComicEffect")
+        
+        var pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+//        pixelBuffer = resizePixelBuffer(pixelBuffer!, width: 256, height: 256)
+        var cameraImage = CIImage(cvImageBuffer: pixelBuffer!)
+        
+        // Crop the image to pass over the style transfer technique
+        cameraImage = cameraImage.cropped(to: CGRect(x: 0, y: 0, width: 256, height: 256))
+        cameraImage = transferStyle(image: cameraImage, model: StarryStyle()) ?? cameraImage
+        
+//        comicEffect!.setValue(cameraImage, forKey: kCIInputImageKey)
+        
+//        let cgImage = self.context.createCGImage(comicEffect!.outputImage!, from: cameraImage.extent)!
+        let cgImage = self.context.createCGImage(cameraImage, from: cameraImage.extent)!
+        
+        DispatchQueue.main.async {
+            let filteredImage = UIImage(cgImage: cgImage)
+            self.filteredImage.image = filteredImage
+        }
+    }
+    
+    // Call ML Methods
+    func transferStyle(image:CIImage, model:StarryStyle) -> CIImage? {
+        let styleArray = try? MLMultiArray(shape: [1] as [NSNumber], dataType: .double)
+        styleArray?[0] = 1.0
+        
+        if let image = pixelBufferFromImage(ciimage: image) {
+            do {
+                let predictionOutput = try model.prediction(image: image, index: styleArray!)
+                
+                let ciImage = CIImage(cvPixelBuffer: predictionOutput.stylizedImage)
+                return ciImage
+            } catch let error as NSError {
+                print("CoreML Model Error: \(error)")
+                return nil
+            }
+        }
+        return nil
+    }
+    
+    func pixelBufferFromImage(ciimage: CIImage) -> CVPixelBuffer? {
+        //let cgimage = convertCIImageToCGImage(inputImage: ciimage!)
+        let tmpcontext = CIContext(options: nil)
+        let cgimage =  tmpcontext.createCGImage(ciimage, from: ciimage.extent)
+        
+        let cfnumPointer = UnsafeMutablePointer<UnsafeRawPointer>.allocate(capacity: 1)
+        let cfnum = CFNumberCreate(kCFAllocatorDefault, .intType, cfnumPointer)
+        let keys: [CFString] = [kCVPixelBufferCGImageCompatibilityKey, kCVPixelBufferCGBitmapContextCompatibilityKey, kCVPixelBufferBytesPerRowAlignmentKey]
+        let values: [CFTypeRef] = [kCFBooleanTrue, kCFBooleanTrue, cfnum!]
+        let keysPointer = UnsafeMutablePointer<UnsafeRawPointer?>.allocate(capacity: 1)
+        let valuesPointer =  UnsafeMutablePointer<UnsafeRawPointer?>.allocate(capacity: 1)
+        keysPointer.initialize(to: keys)
+        valuesPointer.initialize(to: values)
+        
+        let options = CFDictionaryCreate(kCFAllocatorDefault, keysPointer, valuesPointer, keys.count, nil, nil)
+        
+        let width = cgimage!.width
+        let height = cgimage!.height
+        
+        var pxbuffer: CVPixelBuffer?
+        // if pxbuffer = nil, you will get status = -6661
+        var status = CVPixelBufferCreate(kCFAllocatorDefault, width, height,
+                                         kCVPixelFormatType_32BGRA, options, &pxbuffer)
+        status = CVPixelBufferLockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0));
+        
+        let bufferAddress = CVPixelBufferGetBaseAddress(pxbuffer!);
+        
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+        let bytesperrow = CVPixelBufferGetBytesPerRow(pxbuffer!)
+        let context = CGContext(data: bufferAddress,
+                                width: width,
+                                height: height,
+                                bitsPerComponent: 8,
+                                bytesPerRow: bytesperrow,
+                                space: rgbColorSpace,
+                                bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue);
+        context?.concatenate(CGAffineTransform(rotationAngle: 0))
+//        context?.concatenate(__CGAffineTransformMake( 1, 0, 0, -1, 0, CGFloat(height) )) //Flip Vertical
+        //        context?.concatenate(__CGAffineTransformMake( -1.0, 0.0, 0.0, 1.0, CGFloat(width), 0.0)) //Flip Horizontal
+        
+        
+        context?.draw(cgimage!, in: CGRect(x:0, y:0, width:CGFloat(width), height:CGFloat(height)));
+        status = CVPixelBufferUnlockBaseAddress(pxbuffer!, CVPixelBufferLockFlags(rawValue: 0));
+        return pxbuffer!;
+        
+    }
+}
